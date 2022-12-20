2f50fe16c90d0339f399fecf9c6dccce5974ec11
Jordan Ho <jordanho2782@gmail.com>
2022-12-20 12:05:17 +0800

    Complete exercise 1-6

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...element) {
+    count = 0;
+    let arrayToBeSliced = array;
+    while(count < element.length){
+        let index = array.indexOf(element[count])//index of char in array to be removed
+        if (index>=0){
+            arrayToBeSliced.splice(index, 1);
+        }
+        count++
+    }
+    return arrayToBeSliced;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(smallNum, largeNum) {
 
+
+    if((typeof smallNum != "number") || (typeof largeNum != "number")){
+        return "ERROR";
+    }
+
+    if(smallNum<0 || largeNum<0){
+        return "ERROR";
+    }
+
+    if(largeNum<smallNum){
+        let tempSmallNum = smallNum
+        let tempLargeNum = largeNum
+        smallNum = tempLargeNum
+        largeNum = tempSmallNum
+    }
+
+    if(smallNum==largeNum){
+        return smallNum
+    }
+
+    for(let count=smallNum; count<=largeNum; count++){
+        smallNum+=count
+    }
+    return smallNum
 };
 
 // Do not edit below this line
