0bcee958fd366182db608e1ba29169f7f9df2c35
Jordan Ho <jordanho2782@gmail.com>
2022-12-20 09:55:49 +0800

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,25 @@
-const repeatString = function() {
+const repeatString = function(string, repeatCount) {
 
+    if( (typeof string) != "string" ){
+        return "ERROR";
+    }
+
+    if( !Number.isInteger(repeatCount) || repeatCount<0 ){
+        return "ERROR";
+    }
+
+    let currentString = string;
+
+    if(repeatCount>0){
+        for(let currentCount = 1; currentCount < repeatCount; currentCount++){
+            currentString += string;
+        }
+        return currentString;
+    }
+
+    if(repeatCount==0){
+        return "";
+    }
 };
 
 // Do not edit below this line
